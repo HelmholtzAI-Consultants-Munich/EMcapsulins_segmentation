0c14f7dc77a764dd800ac924541464c58fcfdb39
neuronflow <florian.kofler@tum.de>
2022-12-17 02:48:36 +0100

    streamline pp
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- neuronflow/postprocessing.py
+++ neuronflow/postprocessing.py
@@ -51,18 +51,6 @@ def _calculate_circularity(perimeter, area):
     return circularity
 
 
-def _debug_cc3d(numpy_array, debug, stats=False):
-    if debug == True:
-        labels_out, N = cc3d.connected_components(numpy_array, return_N=True)
-        uniques = np.unique(numpy_array)
-        print(uniques)
-        print(N)
-
-        if stats == True:
-            stats = cc3d.statistics(labels_out)
-            print(stats)
-
-
 def postprocess(
     raw_segmentation_file,
     polished_segmentation_file,
@@ -89,8 +77,6 @@ def postprocess(
 
     binary_segmentation = raw_segmentation > 0
 
-    _debug_cc3d(numpy_array=binary_segmentation, debug=debug)
-
     ### 1. PRUNE ###
     if prune_threshold > 0:
         pruned_map = cc3d.dust(
@@ -99,7 +85,6 @@ def postprocess(
             connectivity=26,
             in_place=False,
         )
-        _debug_cc3d(numpy_array=pruned_map, debug=debug)
 
         multi_segmentation = raw_segmentation * pruned_map
         binary_segmentation = binary_segmentation * pruned_map
@@ -112,11 +97,6 @@ def postprocess(
     ### 2. Connected component analysis ###
     components, N = cc3d.connected_components(binary_segmentation, return_N=True)
     print(N)
-
-    if debug == True:
-        print(multi_segmentation.shape)
-        print(np.count_nonzero(multi_segmentation))
-        pre_uniques, pre_counts = np.unique(multi_segmentation, return_counts=True)
 
     ### 3. Majority Voting! ###
     if majority_vote == True:
@@ -136,14 +116,8 @@ def postprocess(
                     most_frequent_value = multimode(
                         extracted_image[extracted_image > 0]
                     )
-                    # print(len(most_frequent_value))
                     if len(most_frequent_value) == 1:
                         multi_segmentation[extracted_image > 0] = most_frequent_value
-
-            # print(uniques)
-            if debug == True:
-                print(np.unique(extracted_image))
-                print(np.count_nonzero(extracted_image))
 
     ### 4. Fill holes ###
     if fill_holes == True:
@@ -157,35 +131,10 @@ def postprocess(
         multi_segmentation = np.expand_dims(multi_segmentation, axis=0)
         multi_segmentation = np.array(hole_fill_tfs(multi_segmentation))
         multi_segmentation = multi_segmentation[0]  # get rid of channel dimension again
-        # print(multi_segmentation)
 
     ### ** Save nifti ** ###
     write_nifti(
         numpy_array=multi_segmentation,
         output_nifti_path=polished_segmentation_file,
     )
-    lala = read_nifti(polished_segmentation_file)
-    print(lala.shape)
-
-    if debug == True:
-        print(multi_segmentation.shape)
-        print(np.count_nonzero(multi_segmentation))
-        uniques, counts = np.unique(multi_segmentation, return_counts=True)
-
-        print("Pre:")
-        print(raw_segmentation.shape)
-        components, pre_N = cc3d.connected_components(raw_segmentation, return_N=True)
-        print(pre_N)
-        for uni, count in zip(pre_uniques, pre_counts):
-            print("unique:", uni, "// count:", count)
-
-        print("Post:")
-        print(multi_segmentation.shape)
-        components, post_N = cc3d.connected_components(
-            multi_segmentation, return_N=True
-        )
-        print(post_N)
-        for uni, count in zip(uniques, counts):
-            print("unique:", uni, "// count:", count)
-
     return True
